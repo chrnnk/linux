5d47f7ea22c302f82ad0023940a493089f4fbe45
Nick Nelson <nicholasadamnelson@gmail.com>
2022-05-22 12:50:29 -0400

    Add files via upload

---
+++ delete-non-english-subs.sh
@@ -0,0 +1,1 @@
+find /mnt/user/data/media -regextype posix-extended ! -regex ".*\.(en|eng)((\.cc|.sdh|.hi|\.forced)|)\.srt\$" | egrep "\.srt" | sed 's/^/rm -f \"/' | sed 's/$/\"/' >> /mnt/user/data/logs/delete-non-eng-srt.sh
