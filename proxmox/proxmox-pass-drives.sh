17294b56c39c8b602e7ed9f76377e2663f9e8657
Nick Nelson <nicholasadamnelson@gmail.com>
2022-05-24 21:33:59 -0400

    Update proxmox-pass-drives.sh

--- proxmox/proxmox-pass-drives.sh
+++ proxmox/proxmox-pass-drives.sh
@@ -4,4 +4,4 @@ qm set 101 -sata4 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JP8Y2
 qm set 101 -sata3 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JPRT5
 qm set 101 -sata2 /dev/disk/by-id/ata-ST8000VN004-2M2101_WKD2HM6K
 qm set 101 -sata1 /dev/disk/by-id/ata-ST8000VN004-2M2101_WSD1V6DN
-qm set 101 -sata6 /dev/disk/by-id/nvme-nvme-Samsung_SSD_950_PRO_512GB_S2GMNX0H508523P
+# qm set 101 -sata6 /dev/disk/by-id/nvme-nvme-Samsung_SSD_950_PRO_512GB_S2GMNX0H508523P
