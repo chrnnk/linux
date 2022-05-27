7b3aeaf3f136ec66e572b238bde1232ac08dd5ef
chronnick <nicholasadamnelson@gmail.com>
2022-05-26 21:49:21 -0400

    Delete proxmox-pass-drives.sh

--- proxmox-pass-drives.sh
+++
@@ -1,6 +0,0 @@
-ls /dev/disk/by-id
-qm set 101 -sata5 /dev/disk/by-id/ata-HITACHI_HUS724040ALE640_PBJE3WLT
-qm set 101 -sata4 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JP8Y2
-qm set 101 -sata3 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JPRT5
-qm set 101 -sata2 /dev/disk/by-id/ata-ST8000VN004-2M2101_WKD2HM6K
-qm set 101 -sata1 /dev/disk/by-id/ata-ST8000VN004-2M2101_WSD1V6DN
