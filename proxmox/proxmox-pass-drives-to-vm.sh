80c18239ba6fa3b5c7db68cc785cae2f1d5a72dd
Nick Nelson <nicholasadamnelson@gmail.com>
2022-06-01 20:04:56 -0400

    Update proxmox-pass-drives-to-vm.sh

--- proxmox/proxmox-pass-drives-to-vm.sh
+++ proxmox/proxmox-pass-drives-to-vm.sh
@@ -1,7 +1,13 @@
 ls /dev/disk/by-id
-qm set 101 -sata5 /dev/disk/by-id/ata-HITACHI_HUS724040ALE640_PBJE3WLT
-qm set 101 -sata4 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JP8Y2
-qm set 101 -sata3 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JPRT5
-qm set 101 -sata2 /dev/disk/by-id/ata-ST8000VN004-2M2101_WKD2HM6K
-qm set 101 -sata1 /dev/disk/by-id/ata-ST8000VN004-2M2101_WSD1V6DN
+qm set 104 -sata5 /dev/disk/by-id/ata-HITACHI_HUS724040ALE640_PBJE3WLT
+qm set 104 -sata4 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JP8Y2
+qm set 104 -sata3 /dev/disk/by-id/ata-ST8000VN0022-2EL112_ZA1JPRT5
+qm set 104 -sata2 /dev/disk/by-id/ata-ST8000VN004-2M2101_WKD2HM6K
+qm set 104 -sata1 /dev/disk/by-id/ata-ST8000VN004-2M2101_WSD1V6DN
 # qm set 101 -sata6 /dev/disk/by-id/nvme-nvme-Samsung_SSD_950_PRO_512GB_S2GMNX0H508523P
+qm set 104 -scsi1 /dev/disk/by-id/ata-ST14000NM000J-2TX103_ZR900KK6
+qm set 104 -scsi2 /dev/disk/by-id/ata-ST14000NM000J-2TX103_ZR900KRS
+qm set 104 -scsi3 /dev/disk/by-id/ata-ST14000NM000J-2TX103_ZR900LPN
+qm set 104 -scsi4 /dev/disk/by-id/ata-WDC_WD140EDGZ-11B1PA0_Y5KYU41C
+qm set 104 -scsi5 /dev/disk/by-id/ata-WDC_WD140EDGZ-11B1PA0_Y6GDZLDC
+qm set 104 -scsi6 /dev/disk/by-id/ata-WDC_WD140EDGZ-11B2DA2_2CJ6V0WN
