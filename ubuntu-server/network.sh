acef7c22bba5e4bba74eaa25648f1930331e9ae5
Nick Nelson <nicholasadamnelson@gmail.com>
2022-06-26 01:43:33 -0400

    Update network.sh

--- ubuntu-server/network.sh
+++ ubuntu-server/network.sh
@@ -1,11 +1,11 @@
 #!
 apt-get install network-tools
 ifconfig | grep mtu
-ifconfig Interface_name> mtu mtu_size> up
+ifconfig interface_name mtu 9000 up
 # test, then to make permanent
 sudo nano /etc/network/interfaces
 # append the below line in the file:
-post-up /sbin/ifconfig ens33 mtu 1300 up
+post-up /sbin/ifconfig ens33 mtu 9000 up
 # save, close
 sudo service networking restart
 sudo ifup interface_name
