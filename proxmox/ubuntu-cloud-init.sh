4851c6e9dc92b7a98f7f434d3c0290ca6e6ab526
chrxnn <nick@chrxnn.com>
2022-07-10 13:43:54 -0400

    asd

--- proxmox/ubuntu-cloud-init.sh
+++ proxmox/ubuntu-cloud-init.sh
@@ -1,7 +1,7 @@
 # Download Ubuntu (replace with the url of the one you chose from above)
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 # Create a new virtual machine
-qm create 9560 --memory 4096 --core 4 --name docker-ubuntu-ci --net0 virtio,bridge=vmbr0
+qm create 9560 --memory 4096 --core 4 --name docker-ubuntu --net0 virtio,bridge=vmbr0
 # Import the downloaded Ubuntu disk to storage
 qm importdisk 9560 jammy-server-cloudimg-amd64.img xerxes-truenas
 # Attach the new disk to the vm as a scsi drive on the scsi controller
@@ -12,9 +12,22 @@ qm set 9560 --ide2 xerxes-truenas:cloudinit
 qm set 9560 --boot c --bootdisk scsi0
 # Add serial console
 qm set 9560 --serial0 socket --vga serial0
+# increase vdisk to 10gb
+qm resize 9560 scsi0 +8G
 # DO NOT START YOUR VM
 # Make changes in proxmox webui for cloud-init settings, set net to dhcp, username, etc
 # Create template
 qm template 9560
 # Clone template
+qm clone 9560 561 --name docker01 --full
+qm clone 9560 562 --name docker02 --full
+qm clone 9560 563 --name docker03 --full
+qm clone 9560 564 --name docker04 --full
+qm clone 9560 565 --name docker05 --full
 qm clone 9560 566 --name docker06 --full
+qm clone 9560 567 --name docker07 --full
+qm clone 9560 568 --name docker08 --full
+qm clone 9560 569 --name docker09 --full
+# Configure static ips on new clones in WEBUI
+# Write code for this later
+# INSIDE VM RUN:
--- ubuntu-server/docker-install.sh
+++ ubuntu-server/docker-install.sh
@@ -3,14 +3,14 @@
 ## UPDATE ALL, REBOOT IF NEEDED
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove
 ## DOCKER SETUP
-sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release -y
 # get the GPG key which is needed to connect to the Docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # add docker repo to APT sources list
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove
+# sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
 # install docker-ce, the full fat docker from the docker repo, not the basic docker.io included in debian repos
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 # check that docker is running
 sudo systemctl status docker
 # verify this command works
