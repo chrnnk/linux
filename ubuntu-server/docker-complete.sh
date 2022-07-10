50c1a0b36654ec18cbd9c428bb9ca0927b0d4af0
Nick Nelson <nick@chrxnn.com>
2022-07-10 12:59:32 -0700

    Update docker-complete.sh

--- ubuntu-server/docker-complete.sh
+++ ubuntu-server/docker-complete.sh
@@ -1,4 +1,4 @@
-#!
+#! bash -c "$(wget -qLO - https://github.com/chrxnn/linux/raw/main/ubuntu-server/docker-complete.sh)"
 bash -c "$(wget -qLO - https://github.com/chrxnn/linux/raw/main/ubuntu-server/docker-install.sh)"
 bash -c "$(wget -qLO - https://github.com/chrxnn/linux/raw/main/ubuntu-server/docker-create-folders.sh)"
 bash -c "$(wget -qLO - https://github.com/chrxnn/linux/raw/main/ubuntu-server/docker-setup.sh)"
