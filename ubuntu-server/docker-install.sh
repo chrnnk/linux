28c00f790eb4169542e863f791386afd08dfdaec
Nick Nelson <nick@chrxnn.com>
2022-07-10 10:07:17 -0700

    Update docker-install.sh

--- ubuntu-server/docker-install.sh
+++ ubuntu-server/docker-install.sh
@@ -2,14 +2,14 @@
 ## SETUP USERS
 # https://github.com/chrxnn/linux/blob/main/ubuntu-server/users-setup.sh
 ## UPDATE ALL, REBOOT IF NEEDED
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
 ## DOCKER SETUP
 sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
 # get the GPG key which is needed to connect to the Docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # add docker repo to APT sources list
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
 # install docker-ce, the full fat docker from the docker repo, not the basic docker.io included in debian repos
 sudo apt install docker-ce
 # check that docker is running
