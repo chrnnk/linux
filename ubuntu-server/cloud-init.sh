4cb89e9a9dfa031e461c8707077e4524d7541abc
Nick Nelson <nicholasadamnelson@gmail.com>
2022-07-09 19:56:53 -0700

    Update cloud-init.sh

--- ubuntu-server/cloud-init.sh
+++ ubuntu-server/cloud-init.sh
@@ -1,11 +1,11 @@
 # Download Ubuntu (replace with the url of the one you chose from above)
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 # Create a new virtual machine
-qm create 9560 --memory 4096 --core 4 --name ubuntu-cloud --net0 virtio,bridge=vmbr0
+qm create 9560 --memory 4096 --core 4 --name docker-ubuntu-ci --net0 virtio,bridge=vmbr0
 # Import the downloaded Ubuntu disk to storage
 qm importdisk 9560 jammy-server-cloudimg-amd64.img xerxes-truenas
 # Attach the new disk to the vm as a scsi drive on the scsi controller
-qm set 9560 --scsihw virtio-scsi-pci --scsi0 xerxes-truenas:vm-9560-disk-0.raw
+qm set 9560 --scsihw virtio-scsi-pci --scsi0 xerxes-truenas:9560/vm-9560-disk-0.raw
 # Add cloud init drive
 qm set 9560 --ide2 xerxes-truenas:cloudinit
 # Make the cloud init drive bootable and restrict BIOS to boot from disk only
