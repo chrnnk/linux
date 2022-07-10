72d850c5d0d0315b3445b0f4ffc6ab17e23e19b8
Nick Nelson <nick@chrxnn.com>
2022-07-09 22:23:05 -0400

    Update setup-users.sh

--- ubuntu-server/setup-users.sh
+++ ubuntu-server/setup-users.sh
@@ -1,11 +1,12 @@
 #!
 ## Change password after initial setup
 passwd
+## Add your user to sudo group
+sudo usermod -aG sudo USERNAME
 ## ALLOW YOUR USER TO RUN SUDO WITHOUT PASSWORD PROMPT:
 sudo EDITOR=nano visudo
 # add the following line to the end of the file:
 ```
 USERNAME    ALL=(ALL) NOPASSWD:ALL
 ```
-## UPDATE ALL, REBOOT IF NEEDED
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+## Log out and back in, test by running a command that previously needed sudo
