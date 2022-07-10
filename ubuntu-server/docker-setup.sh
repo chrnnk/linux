4296349ed64663dacee73ab6dec06087eacac9dc
Nick Nelson <nick@chrxnn.com>
2022-07-10 11:54:35 -0700

    Update docker-setup.sh

--- ubuntu-server/docker-setup.sh
+++ ubuntu-server/docker-setup.sh
@@ -1,5 +1,5 @@
 #! bash -c "$(wget -qLO - https://github.com/chrxnn/linux/raw/main/ubuntu-server/docker-setup.sh)"
-sudo apt install acl
+sudo apt install acl -y
 sudo chmod 775 /home/$USER/docker
 sudo setfacl -Rdm g:docker:rwx /home/$USER/docker
 sudo setfacl -Rm g:docker:rwx /home/$USER/docker
