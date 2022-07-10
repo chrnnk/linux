cdc4a94631ce81818343ea4ceb9b5aa30fbd76b1
chrxnn <nick@chrxnn.com>
2022-07-10 14:03:34 -0400

    Update docker-install.sh

--- ubuntu-server/docker-install.sh
+++ ubuntu-server/docker-install.sh
@@ -9,7 +9,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # add docker repo to APT sources list
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
 # install docker-ce, the full fat docker from the docker repo, not the basic docker.io included in debian repos
 sudo apt install docker-ce -y
 # check that docker is running
