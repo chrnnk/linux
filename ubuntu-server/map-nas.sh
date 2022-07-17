39c8558f5cd3b477a540f8448a73bd0c8a6018ff
Nick Nelson <nick@chrxnn.com>
2022-07-16 23:14:45 -0700

    Update map-nas.sh

--- ubuntu-server/map-nas.sh
+++ ubuntu-server/map-nas.sh
@@ -2,5 +2,5 @@
 cd /mnt/
 sudo mkdir nas
 sudo apt install nfs-common -y
-sudo su -c "echo '10.10.50.81:/mnt/pool1/media /mnt/nas nfs default 0 0' >> /etc/fstab"
+sudo su -c "echo 'nas:/mnt/pool1/media /mnt/nas nfs default 0 0' >> /etc/fstab"
 sudo mount -a
