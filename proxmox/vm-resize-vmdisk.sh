4036edf7572be64366a98b86f9aab6cf92dbf9a2
Nick Nelson <nick@chrxnn.com>
2022-07-16 22:30:51 -0700

    Update vm-resize-vmdisk.sh

--- proxmox/vm-resize-vmdisk.sh
+++ proxmox/vm-resize-vmdisk.sh
@@ -1,3 +1,9 @@
 qm resize 9560 scsi0 +7.988G
 # inside linux vm if running lvm
+lvdisplay 
 sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
+lvdisplay 
+cd /
+df -h
+resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+df -h
