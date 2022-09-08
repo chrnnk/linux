58351e262e110bf000feb9c98027f5af75b0c883
Nick Nelson <nick@chrxnn.com>
2022-09-08 16:36:34 -0400

    Create esxi-snmp.sh

---
+++ esxi/esxi-snmp.sh
@@ -0,0 +1,10 @@
+# ESXi 7.0
+esxcli system snmp set --communities YOUR_COMMUNITY_STRING
+esxcli system snmp set --enable true
+# ESXi 6.x
+esxcli system snmp set -r
+esxcli system snmp set -c YOUR_STRING
+esxcli system snmp set -p 161
+esxcli system snmp set -L "City, State, Country"
+esxcli system snmp set -C noc@example.com
+esxcli system snmp set -e yes
