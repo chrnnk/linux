1ea404bfcb390d51bf2908b47b1cabda921f1273
chrxnn <nick@chrxnn.com>
2022-09-15 00:38:42 -0400

    qcow2 adds

--- convert-vmdk-to-kvm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-qemu-img convert -f vmdk /mnt/pve/unraid-data/DC.chronnick.local/DC.chronnick.local.vmdk -O qcow2 /mnt/pve/unraid-data/DC.chronnick.local/DC.chronnick.local.qcow2
---
+++ esxi/convert-vmdk-to-qcow.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+qemu-img convert -f vmdk /mnt/pve/unraid-data/DC.chronnick.local/DC.chronnick.local.vmdk -O qcow2 /mnt/pve/unraid-data/DC.chronnick.local/DC.chronnick.local.qcow2
