fe5762f71f5e2b7725db8c18c74039c07161574d
Nick Nelson <nick@chrxnn.com>
2022-09-16 21:03:20 -0400

    Update cluster-delete-node.sh

--- proxmox/cluster-delete-node.sh
+++ proxmox/cluster-delete-node.sh
@@ -1,5 +1,5 @@
 #!
-# first of all, stop doing that
+# first of all, stop breaking the cluster
 pvecm status
 # command below sets the quorom to 1 (if you had 2 nodes. if you have 3 nodes and 2 are still online set this to 2)
 pvecm expected 1
