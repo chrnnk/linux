bda75baae9593704d55185cc1fb76ad1f81eeb88
Nick Nelson <nick@chrxnn.com>
2022-09-16 21:00:31 -0400

    Delete updates.sh

--- proxmox/updates.sh
+++
@@ -1,7 +0,0 @@
-#!
-# ttek's PVE7 post install script
-bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/misc/post-install-v3.sh)"
-# ttek's LXC updater
-bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/misc/update-lxcs.sh)"
-# linux update all
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
