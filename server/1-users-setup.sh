ce740cfa36bb9b340b8fae8dedf2ceedffb56a69
Nick Nelson <nick@chrxnn.com>
2022-10-29 01:48:40 -0400

    Update 1-users-setup.sh

--- server/1-users-setup.sh
+++ server/1-users-setup.sh
@@ -4,9 +4,7 @@ passwd
 ## Add your user to sudo group if you can't sudo
 # sudo usermod -aG sudo USERNAME
 ## ALLOW YOUR USER TO RUN SUDO WITHOUT PASSWORD PROMPT:
-sudo EDITOR=nano visudo
+# sudo EDITOR=nano visudo
 # add the following line to the end of the file:
-```
-USERNAME    ALL=(ALL) NOPASSWD:ALL
-```
+# USERNAME    ALL=(ALL) NOPASSWD:ALL
 ## Log out and back in, test by running a command that previously needed sudo
