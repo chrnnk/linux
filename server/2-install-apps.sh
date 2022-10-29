792823aaa90c393c0db7636036f459a195f76df9
Nick Nelson <nick@chrxnn.com>
2022-10-29 17:17:37 -0400

    Delete 2-install-apps.sh

--- server/2-install-apps.sh
+++
@@ -1,9 +0,0 @@
-# wget -O - https://raw.githubusercontent.com/chrxnn/linux/main/server/2-install-apps.sh | bash
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt install nala -y && sudo apt install qemu-guest-agent -y && sudo apt install neofetch -y
-mkdir ~/github
-cd ~/github
-git clone https://github.com/chrxnn/linux
-chmod +x ${HOME}/github/linux/bash/setup.sh
-${HOME}/github/linux/bash/setup.sh
-# sudo nala fetch
-# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
