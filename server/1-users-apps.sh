acb740fd8e72af0586daef9f40434aab73160ee0
Nick Nelson <nick@chrxnn.com>
2022-10-29 18:10:19 -0400

    Update 1-users-apps.sh

--- server/1-users-apps.sh
+++ server/1-users-apps.sh
@@ -4,10 +4,11 @@ echo "${USER}  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers
 
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
 sudo apt install nala -y && sudo apt install qemu-guest-agent -y && sudo apt install neofetch -y
-# sudo nala fetch --country US
 
 mkdir ~/github
 cd ~/github
 git clone https://github.com/chrxnn/linux
 chmod +x ${HOME}/github/linux/bash/setup.sh
 ${HOME}/github/linux/bash/setup.sh
+
+sudo nala fetch --country US
