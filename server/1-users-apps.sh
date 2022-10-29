d14c8606761728c8ab93cad9e429a0c173f6c681
Nick Nelson <nick@chrxnn.com>
2022-10-29 17:17:03 -0400

    Update and rename 1-users-setup.sh to 1-users-apps.sh

---
+++ server/1-users-apps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash # wget -O - https://raw.githubusercontent.com/chrxnn/linux/main/server/2-install-apps.sh | bash
+
+echo "${USER}  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers
+
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+sudo apt install nala -y && sudo apt install qemu-guest-agent -y && sudo apt install neofetch -y
+sudo nala fetch --country US
+
+mkdir ~/github
+cd ~/github
+git clone https://github.com/chrxnn/linux
+chmod +x ${HOME}/github/linux/bash/setup.sh
+${HOME}/github/linux/bash/setup.sh
--- server/1-users-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "${USER}  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers
