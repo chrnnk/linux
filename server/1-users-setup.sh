ae725d16a5728a7d7a0596807ae3041fafab2181
Nick Nelson <nick@chrxnn.com>
2022-10-29 17:08:06 -0400

    Update 1-users-setup.sh

--- server/1-users-setup.sh
+++ server/1-users-setup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 echo "${USER}  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers
-## ALLOW YOUR USER TO RUN SUDO WITHOUT PASSWORD PROMPT:
-# sudo EDITOR=nano visudo
-# add the following line to the end of the file:
-# USERNAME    ALL=(ALL) NOPASSWD:ALL
-## Log out and back in, test by running a command that previously needed sudo
