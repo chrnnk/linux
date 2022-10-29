eaddc8f2e5b9340a6abef6f044bcfb85f4b2ffa8
Nick Nelson <nick@chrxnn.com>
2022-10-29 11:32:11 -0400

    Update 2-install-apps.sh

--- server/2-install-apps.sh
+++ server/2-install-apps.sh
@@ -1,8 +1,9 @@
 sudo apt update && sudo apt install nala -y
-cd ~
-mkdir github
-cd github
+mkdir ~/github
+cd ~/github
 git clone https://github.com/chrxnn/linux
+chmod +x ./linus/bash/setup.sh
+./setup.sh
 sudo nala fetch
 # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 ## edit both files below and add the following lines
