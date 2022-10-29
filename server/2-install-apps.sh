b5ad944b30deb8f250bff673fd0637403662e413
Nick Nelson <nick@chrxnn.com>
2022-10-29 00:14:30 -0400

    Update and rename 2-install-nala.sh to 2-install-apps.sh

---
+++ server/2-install-apps.sh
@@ -0,0 +1,24 @@
+sudo apt update && sudo apt install nala -y
+cd ~
+mkdir github
+cd github
+git clone https://github.com/chrxnn/linux
+sudo nala fetch
+# 1 2 3 4 5 6 7 8 9 10
+## edit both files below and add the following lines
+#sudo nano ~/.bashrc
+#sudo nano /root/.bashrc
+## add the below
+## convert apt to nala
+#apt() { 
+#  command nala "$@"
+#}
+#sudo() {
+#  if [ "$1" = "apt" ]; then
+#    shift
+#    command sudo nala "$@"
+#  else
+#    command sudo "$@"
+#  fi
+#}
+## save and exit
