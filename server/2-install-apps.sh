bed62904abdd7a5f45754079532ec8d1fab8f50e
Nick Nelson <nick@chrxnn.com>
2022-10-29 12:24:14 -0400

    Update 2-install-apps.sh

--- server/2-install-apps.sh
+++ server/2-install-apps.sh
@@ -6,3 +6,21 @@ git clone https://github.com/chrxnn/linux
 # chmod +x ./linux/bash/setup.sh
 # ./linux/bash/setup.sh
 # sudo nala fetch
+# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+## edit both files below and add the following lines
+#sudo nano ~/.bashrc
+#sudo nano /root/.bashrc
+## add the below
+## convert apt to nala
+#apt() { 
+#  command nala "$@"
+#}
+#sudo() {
+#  if [ "$1" = "apt" ]; then
+#    shift
+#    command sudo nala "$@"
+#  else
+#    command sudo "$@"
+#  fi
+#}
+## save and exit
