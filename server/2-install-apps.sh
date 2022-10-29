16866187a698fe2e1dd7d110f1b315a618e978f1
Nick Nelson <nick@chrxnn.com>
2022-10-29 12:15:37 -0400

    Update 2-install-apps.sh

--- server/2-install-apps.sh
+++ server/2-install-apps.sh
@@ -4,7 +4,7 @@ mkdir ~/github
 cd ~/github
 git clone https://github.com/chrxnn/linux
 chmod +x ./linux/bash/setup.sh
-./setup.sh
+./linux/bash/setup.sh
 sudo nala fetch
 # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 ## edit both files below and add the following lines
