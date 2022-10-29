2bd987e6cb106780dcbd703446f56adfd888459c
Nick Nelson <nick@chrxnn.com>
2022-10-29 16:47:40 -0400

    Update 2-install-apps.sh

--- server/2-install-apps.sh
+++ server/2-install-apps.sh
@@ -4,6 +4,7 @@ mkdir ~/github
 cd ~/github
 git clone https://github.com/chrxnn/linux
 # chmod +x ./linux/bash/setup.sh
-# ./linux/bash/setup.sh
+chmod +x ${HOME}/github/linux/bash/setup.sh
+${HOME}/github/linux/bash/setup.sh
 # sudo nala fetch
 # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
