638c09d0f65c79fb9264a2695ba375236a376c68
Nick Nelson <nick@chrxnn.com>
2022-12-26 12:40:17 -0500

    Update plex-migrate-appdata.sh

--- scripts/plex-migrate-appdata.sh
+++ scripts/plex-migrate-appdata.sh
@@ -1,5 +1,8 @@
 # Stop the plexmediaserver service on your old plexmediaserver and your new plexmediaserver
 sudo systemctl stop plexmediaserver
+# Delete the existing library on the NEW server before migrating
+sudo rm -rf /var/lib/plexmediaserver/Library
+# Start rsync
 sudo rsync -azvh nick@plex:/var/lib/plexmediaserver/ /var/lib/plexmediaserver/
 # Upon completion, change the owner of the plexmediaserver directory to the plex user on your new plexmediaserver, Plex on Ubuntu requires this user to be the owner of the directory
 sudo chown -R plex:plex /var/lib/plexmediaserver/
