1cc6523b30fe58b4e0e62939eca46282c61c84fa
Nick Nelson <nick@chrxnn.com>
2022-12-26 10:36:31 -0500

    Create plex-change-appdata.sh

---
+++ scripts/plex-change-appdata.sh
@@ -0,0 +1,13 @@
+# Source: symlink plex
+# What you wanna do is stop the Plex service On systemd it's
+sudo systemd stop plexmediaserver.service
+# Then copy the Plex data for to a new place
+cp -rf /var/lib/plexmediaserver/Library/Application\ Support /new/path
+# Backup the old stuff In-case
+mv /var/lib/plexmediaserver/Library/Application\ Support /var/lib/plexmediaserver/Library/Application\ Support.OLD
+# Then do
+ln -s /new/Plex/path/Application\ Support /var/lib/plexmediaserver/Library/Application\ Support
+# Then chown the new path
+sudo chown -R plex:plex /var/lib/plexmediaserver/Library/Application\ Support
+# Then restart plex
+sudo systemctl start plexmediaserver.service
