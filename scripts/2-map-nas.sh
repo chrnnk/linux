d4957d7186c3b20a767b38de698bc4b155951881
Nick Nelson <nick@chrxnn.com>
2023-02-08 21:33:32 -0800

    Update 2-map-nas.sh

--- scripts/2-map-nas.sh
+++ scripts/2-map-nas.sh
@@ -5,5 +5,4 @@ cd /mnt/
 sudo mkdir media
 sudo apt install nfs-common -y
 sudo su -c "echo 'unraid.chrxnn.lan:/mnt/user/media/ /mnt/media nfs default 0 0' >> /etc/fstab"
-sudo su -c "echo 'unraid.chrxnn.lan:/mnt/user/downloads/ /mnt/downloads nfs default 0 0' >> /etc/fstab"
 sudo mount -a
