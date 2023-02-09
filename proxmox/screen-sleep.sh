9e515e744731381ea8b41a9b337b3aaaa67d3dbb
Nick Nelson <nick@chrxnn.com>
2023-02-09 07:31:46 -0800

    Update screen-sleep.sh

--- proxmox/screen-sleep.sh
+++ proxmox/screen-sleep.sh
@@ -3,4 +3,10 @@ setterm -term linux -blank 1 -powersave powerdown -powerdown 1 </dev/tty1 >/dev/
 # save to /root/screen-sleep.sh
 # chmod +x ./screen-sleep.sh
 # crontab -e
-# @reboot /root/screen-sleep.sh
+@reboot /root/screen-sleep.sh
+# 0 * * * * /root/screen-sleep.sh
+# 10 * * * * /root/screen-sleep.sh
+# 20 * * * * /root/screen-sleep.sh
+# 30 * * * * /root/screen-sleep.sh
+# 40 * * * * /root/screen-sleep.sh
+# 50 * * * * /root/screen-sleep.sh
