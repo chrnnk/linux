9415bf616b122ac71d974cfb23ed7c3990b34147
Nick Nelson <nick@chrxnn.com>
2023-02-09 03:37:36 -0500

    fix 3am typos

--- proxmox/screen-sleep.sh
+++ proxmox/screen-sleep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 setterm -term linux -blank 1 -powersave powerdown -powerdown 1 </dev/tty1 >/dev/tty1
 # save to /root/screen-sleep.sh
-# chmod +x ./screen-sleep
+# chmod +x ./screen-sleep.sh
 # crontab -e
-# @reboot /root/down_monitor.sh
+# @reboot /root/screen-sleep.sh
