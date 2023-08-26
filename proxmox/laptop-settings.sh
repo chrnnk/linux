a5c886b43aec729863b3b7d52252b8516a7cb9fa
Nick Nelson <nick@chrxnn.com>
2023-08-26 18:13:28 -0400

    Update laptop-settings.sh
    add systemd-boot

--- proxmox/laptop-settings.sh
+++ proxmox/laptop-settings.sh
@@ -1,5 +1,6 @@
 #!
 # below code will turn off laptop screen after 20 seconds of inactivity
+# GRUB
 nano /etc/default/grub
 GRUB_CMDLINE_LINUX = 'consoleblank=20'
 update-grub
@@ -11,3 +12,8 @@ HandleLidSwitchDocked=ignore
 systemctl restart systemd-logind
 reboot
 # go here https://github.com/chrxnn/linux/blob/main/proxmox/screen-sleep.sh
+# SYSTEMD-BOOT
+nano /etc/kernel/cmdline
+# add consoleblank=20 to end
+proxmox-boot-tool refresh
+reboot
