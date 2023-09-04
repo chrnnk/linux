2ab9670be07a6edab48634f153452130c873e4cc
Nick Nelson <nick@chrxnn.com>
2023-09-04 10:23:11 -0400

    Delete cron-rsync.sh

--- cron-rsync.sh
+++
@@ -1,9 +0,0 @@
-# 00 */1 * * * rsync -a --delete /source/ /destination/backup/hourly
-# 00 17 * * * rsync -a --delete /source/ /destination/backup/daily
-# 00 18 * * 5 rsync -a --delete  /source/ /destination/backup/weekly
-# 00 19 1 * * rsync -a --delete /source/ /destination/backup/monthly_$(date +%Y%m)
-# This performs 4 backups:
-#     * an hourly backup
-#     * a daily backup every day at 5 PM
-#     * a weekly backup every Friday (day 5) at 6PM
-#     * a monthly backup on the 1st at 7PM
