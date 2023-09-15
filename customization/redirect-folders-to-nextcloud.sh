d8863f49ba6ae61353b89edc1cc7b24aaf055e4b
Nick Nelson <nick@chrxnn.com>
2023-09-15 11:18:52 -0400

    Update redirect-folders-to-nextcloud.sh

--- customization/redirect-folders-to-nextcloud.sh
+++ customization/redirect-folders-to-nextcloud.sh
@@ -1,2 +1,8 @@
 #!
 ln -s ~/Nextcloud/Documents ~/
+sudo ln -s /home/nick/Nextcloud/Desktop /home/nick
+sudo ln -s /home/nick/Nextcloud/Documents /home/nick
+sudo ln -s /home/nick/Nextcloud/Downloads /home/nick
+sudo ln -s /home/nick/Nextcloud/Pictures /home/nick
+sudo ln -s /home/nick/Nextcloud/Music /home/nick
+sudo ln -s /home/nick/Nextcloud/GitHub /home/nick
