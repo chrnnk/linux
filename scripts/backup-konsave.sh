a2b8b119a6e405bc440b6daae4be9778f7aa94ea
Nick Nelson <nick@chrxnn.com>
2023-09-17 10:53:33 -0400

    Update backup-konsave.sh
    update var name

--- scripts/backup-konsave.sh
+++ scripts/backup-konsave.sh
@@ -1,3 +1,3 @@
 #!
-_now=$(date +"%Y_%m_%d-%H%M")
-/home/nick/.local/bin/konsave -e main -d /mnt/backups/linux/shodan/konsave/ -n $_now
+date_time=$(date +"%Y_%m_%d-%H%M")
+/home/nick/.local/bin/konsave -e main -d /mnt/backups/linux/shodan/konsave/ -n $date_time
