a7c7d79ba00997f69171b0626aeb9d465bb2e9a0
Nick Nelson <nick@chrxnn.com>
2023-10-08 17:47:40 -0400

    Update map-nas.sh

--- scripts/map-nas.sh
+++ scripts/map-nas.sh
@@ -1,8 +1,10 @@
 #!
 # wget -O - https://raw.githubusercontent.com/chrxnn/linux/main/scripts/2-map-nas.sh | bash
-
-cd /mnt/
-sudo mkdir media
-sudo apt install nfs-common -y
-sudo su -c "echo 'unraid.chrxnn.lan:/mnt/user/media/ /mnt/media nfs default 0 0' >> /etc/fstab"
+sudo mkdir /mnt/media /mnt/tdarr /mnt/backups /mnt/games /mnt/data
+sudo su -c "echo 'xerxes-unraid.home.chrnnk.tv:/mnt/user/media/    /mnt/media    nfs default 0 0' >> /etc/fstab"
+sudo su -c "echo 'xerxes-unraid.home.chrnnk.tv:/mnt/user/media/    /mnt/media    nfs default 0 0' >> /etc/fstab"
+sudo su -c "echo 'xerxes-unraid.home.chrnnk.tv:/mnt/user/tdarr/    /mnt/tdarr    nfs default 0 0' >> /etc/fstab"
+sudo su -c "echo 'xerxes-unraid.home.chrnnk.tv:/mnt/user/backups/  /mnt/backups  nfs default 0 0' >> /etc/fstab"
+sudo su -c "echo 'xerxes-unraid.home.chrnnk.tv:/mnt/user/games/    /mnt/games    nfs default 0 0' >> /etc/fstab"
+sudo su -c "echo 'xerxes-unraid.home.chrnnk.tv:/mnt/user/data/     /mnt/media    nfs data 0 0' >> /etc/fstab"
 sudo mount -a
