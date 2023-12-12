9c32158dd55849d88093016f3caa4eb985190ab6
Nick Nelson <nick@chrnnk.tv>
2023-12-11 21:26:44 -0500

    update paths

--- unraid/userscript-fix-perms.sh
+++ unraid/userscript-fix-perms.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# find /mnt/user/data/ \! -perm 0755 >> /mnt/user/data/logs/non-755-files.log
-/usr/local/sbin/newperms
+# find /mnt/user/media/ \! -perm 0755 >> /mnt/user/media/logs/files.log
+/usr/local/sbin/newperms /mnt/user/media/
