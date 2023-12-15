bb9ba69b14173bce2cb6e7a70d21f4a3303d46e9
Nick Nelson <nick@chrnnk.tv>
2023-12-15 16:59:40 -0500

    Update esxi-snmp.sh
    add 8.0

--- esxi/esxi-snmp.sh
+++ esxi/esxi-snmp.sh
@@ -1,9 +1,12 @@
+# ESXi 8.0
+esxcli system snmp set -c=YOUR_COMMUNITY_STRING
+esxcli system snmp set -e=true
 # ESXi 7.0
 esxcli system snmp set --communities YOUR_COMMUNITY_STRING
 esxcli system snmp set --enable true
 # ESXi 6.x
 esxcli system snmp set -r
-esxcli system snmp set -c YOUR_STRING
+esxcli system snmp set -c YOUR_COMMUNITY_STRING
 esxcli system snmp set -p 161
 esxcli system snmp set -L "City, State, Country"
 esxcli system snmp set -C noc@example.com
