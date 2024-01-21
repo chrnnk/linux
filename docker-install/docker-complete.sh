3773befe749b78ce511142a9f58cab9e46db975d
Nick Nelson <nick@chrnnk.tv>
2024-01-20 22:02:53 -0500

    Delete docker-complete.sh

--- docker-install/docker-complete.sh
+++
@@ -1,4 +0,0 @@
-#! bash -c "$(wget -qLO - https://github.com/chrnnk/linux/raw/main/ubuntu-server/docker-complete.sh)"
-bash -c "$(wget -qLO - https://github.com/chrnnk/linux/raw/main/ubuntu-server/docker-install.sh)"
-bash -c "$(wget -qLO - https://github.com/chrnnk/linux/raw/main/ubuntu-server/docker-create-folders.sh)"
-bash -c "$(wget -qLO - https://github.com/chrnnk/linux/raw/main/ubuntu-server/docker-setup.sh)"
