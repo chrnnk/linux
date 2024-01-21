ad37e320d3628e7200122a244d5f35efee5c40ef
Nick Nelson <nick@chrnnk.tv>
2024-01-20 21:28:25 -0500

    add curl install

--- docker-install/docker-install.sh
+++ docker-install/docker-install.sh
@@ -2,17 +2,26 @@
 # wget -O - https://raw.githubusercontent.com/chrnnk/linux/main/docker-install/docker-install.sh | bash
 ## UPDATE EVERYTHING
 printf '\nUpdating everything\n\n'
+sleep 1
 apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y
 printf '\nEverything successfully updated\n\n'
 sleep 5
+## PREREQ INSTALL
+printf '\nInstalling Prerequisites\n\n'
+sleep 1
+apt install curl -y
+printf '\nPrerequisites installed successfully\n\n'
+sleep 5
 ## DOCKER INSTALL
 printf '\nInstalling Docker\n\n'
+sleep 1
 curl -fsSL https://get.docker.com -o get-docker.sh
 wget -O - https://raw.githubusercontent.com/chrnnk/linux/main/scripts/docker-install.sh | bash
 printf '\nDocker installed successfully\n\n'
 sleep 5
 ## DOCKER COMPOSE INSTALL
 printf '\nInstalling Docker Compose\n\n'
+sleep 1
 COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -20,6 +29,9 @@ curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/cont
 printf '\nDocker Compose installed successfully\n\n'
 sleep 5
 # check that docker is running and working
+printf '\nCheck that Ddocker and Docker Compose are running and working\n\n'
+sleep 1
 docker ps -a
 docker run hello-world
 docker-compose -v
+sleep 5
