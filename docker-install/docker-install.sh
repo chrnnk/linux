594d85ccc1ea803217d6fec5459aa9612ae4465e
Nick Nelson <nick@chrnnk.tv>
2024-01-20 21:18:13 -0500

    Update docker-install.sh

--- docker-install/docker-install.sh
+++ docker-install/docker-install.sh
@@ -1,34 +1,25 @@
 #!
 # wget -O - https://raw.githubusercontent.com/chrnnk/linux/main/scripts/docker-install.sh | bash
-
-## UPDATE ALL, REBOOT IF NEEDED
+## UPDATE EVERYTHING
+printf '\nUpdating everything\n\n'
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
-
-## DOCKER SETUP
-sudo apt install ca-certificates curl gnupg lsb-release -y
-
-# get the GPG key which is needed to connect to the Docker repository
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-# add docker repo to APT sources list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
-
-# install docker-ce, the full fat docker from the docker repo, not the basic docker.io included in debian repos
-sudo apt install docker-ce -y
-
-# check that docker is running
-# sudo systemctl status docker
-# verify this command works
+printf '\nEverything successfully updated\n\n'
+sleep 5
+## DOCKER INSTALL
+printf '\nInstalling Docker\n\n'
+curl -fsSL https://get.docker.com -o get-docker.sh
+wget -O - https://raw.githubusercontent.com/chrnnk/linux/main/scripts/docker-install.sh | bash
+printf '\nDocker installed successfully\n\n'
+sleep 5
+## DOCKER COMPOSE INSTALL
+printf '\nInstalling Docker Compose\n\n'
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+printf '\nDocker Compose installed successfully\n\n'
+sleep 5
+# check that docker is running and working
 sudo docker ps -a
 sudo docker run hello-world
-
-# add user to docker group
-sudo usermod -aG docker ${USER}
-sudo reboot
-
-# AFTER REBOOT CHECK FOLLOWING:
-# DOCKER AUTOSTART AFTER REBOOT
-# sudo systemctl status docker
-# Test running docker commands without sudo
-# docker ps -a
+docker-compose -v
