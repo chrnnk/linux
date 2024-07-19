afd005e27400162cbd7dc3e899d1f19240aadd27
Nick Nelson <nick@chrnnk.tv>
2024-07-19 17:25:26 -0400

    updating

--- scripts/backup-konsave.sh
+++ scripts/backup-konsave.sh
@@ -1,3 +1,3 @@
-#!
+#!/bin/bash
 date_time=$(date +"%Y_%m_%d-%H%M")
 /home/nick/.local/bin/konsave -e main -d /mnt/backups/linux/shodan/konsave/ -n $date_time
--- scripts/copy-directory-names-no-files.sh
+++ scripts/copy-directory-names-no-files.sh
@@ -1,2 +1,2 @@
-#!
+#!/bin/bash
 cd /path/to/directories && find . -type d -exec mkdir -p -- /mnt/pool1/media/movies/1080p/{} \;
